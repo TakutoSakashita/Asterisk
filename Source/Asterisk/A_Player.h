baf386c61f149f1a36e7ad5ef20eb621429c6005
TakutoSakashita <>
2024-05-18 18:31:24 +0900

    init

---
+++ Source/Asterisk/A_Player.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "A_CharacterBase.h"
+#include "A_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTERISK_API AA_Player : public AA_CharacterBase
+{
+	GENERATED_BODY()
+	
+};
